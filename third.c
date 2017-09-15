e31f37ec39dfa4bb6d9f7e51c5a9227adb8cd5a1
liyuank <12709879+liyuank@users.noreply.github.com>
2017-09-14 22:59:22 -0700

    Create third.c

---
+++ third.c
@@ -0,0 +1,34 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void    find_prime_factors(int n)
+{
+    int i;
+    int *ary;
+    int index;
+
+    i = 2;
+    index = 0;
+    ary =(int *)malloc(sizeof(*ary) * index);
+
+    while (i * i <= n)
+    {
+        if ( n % i == 0)
+        {
+            ary[index] = i;
+            i++;
+            index++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+    int j;
+    j = 0;
+    while ( j <= index)
+    {
+        printf("%d ", index);
+        j++;
+    }
+}
