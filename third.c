d0c17553a74bcafeb0a100366559ea93cc02a365
liyuank <12709879+liyuank@users.noreply.github.com>
2017-09-16 22:49:33 -0700

    Update third.c

--- third.c
+++ third.c
@@ -32,6 +32,44 @@ void    find_prime_factors(int n)
         j++;
     }
 }
+int    *find_prime_factors(int n)
+{
+    int i;
+    int *ary;
+    int index;
+
+    i = 2;
+    index = 0;
+    ary =(int *)malloc(sizeof(*ary) * index);
+    int stop = n;
+
+    while (i <= stop)
+    {
+        if ( n % i == 0)
+        {
+            ary[index] = i;
+            //printf("%d ", i);
+            n = n / i;
+
+            index++;
+        }
+        else
+        {
+            i++;
+        }
+    }
+   // printf("index is : %d\n", index);
+
+    // int j;
+    // j = 0;
+    // while ( j < index)
+    // {
+
+    //     printf("%d ", ary[j]);
+    //     j++;
+    // }
+    return(ary);
+}
 
 int main(int argc, char **argv)
 {
